77275e6b3e9362a40c67a33c8a999b471612501f
Myota <allister.wylie@mohawkcollege.ca>
2025-11-06 22:40:44 -0500

    Add new Unity scene & create flipper code

--- unity-project/mini-game-collection/Assets/2025/Team03/Scripts/CarMovement.cs
+++ unity-project/mini-game-collection/Assets/2025/Team03/Scripts/CarMovement.cs
@@ -1,9 +1,12 @@
+using MiniGameCollection;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
+    public bool isB1Pressed = false;
+    public bool isB2Pressed = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class CarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ArcadeInput.Player1.Action1.Pressed && isB1Pressed == false)
+        {
+            isB1Pressed = true;
+            isB2Pressed = false;
+        }
+
+        if (ArcadeInput.Player1.Action2.Pressed && isB2Pressed == false)
+        {
+            isB2Pressed = true;
+            isB1Pressed = false;
+        }
     }
 }
--- unity-project/mini-game-collection/Assets/2025/Team03/Scripts/TrainMovement.cs
+++ unity-project/mini-game-collection/Assets/2025/Team03/Scripts/TrainMovement.cs
@@ -1,9 +1,12 @@
+using MiniGameCollection;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrainMovement : MonoBehaviour
 {
+    public bool isLeftPulled = false;
+    public bool isRightPulled = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class TrainMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ArcadeInput.Player2.Left.Pressed && isLeftPulled == false)
+        {
+            isLeftPulled = true;
+            isRightPulled = false;
+        }
+
+        if (ArcadeInput.Player2.Right.Pressed && isRightPulled == false)
+        {
+            isLeftPulled = false;
+            isRightPulled = true;
+        }
     }
 }
